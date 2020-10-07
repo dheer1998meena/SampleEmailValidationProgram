58e56343bc54de117a87f41eb4a004dc0ccbda4f
dheer meena <dheer1998meena@gmail.com>
2020-10-07 16:48:47 +0530

    [Dheer] Add . SampleEmailValidationProgram

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace EmailValidationProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Email :");
+            string email = Console.ReadLine();
+            RegexValidation reg = new RegexValidation();
+            reg.EmailValidation(email);
+        }
+    }
+}
---
+++ RegexValidation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace EmailValidationProgram
+{
+    class RegexValidation
+    {
+        public void EmailValidation(string email)
+        {
+            Regex regex = new Regex("^[a-zA-Z0-9]+([.+_-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})?$");
+
+            if (regex.IsMatch(email))
+            {
+                Console.WriteLine("The Email is valid");
+            }
+            else
+            {
+                Console.WriteLine("The Email is not valid");
+            }
+
+        }
+
+    }
+}
